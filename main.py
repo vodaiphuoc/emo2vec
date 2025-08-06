a1c2f6e6be4b30f4350894f7bc27135608b77978
vodaiphuoc <phuocvodn98@gmail.com>
2025-08-06 16:09:51 +0700

    update

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from model.load_pretrained_model import download_weigth_from_hf
 
 
-download_weigth_from_hf()
+path= download_weigth_from_hf()
+
+import torch
+
+with open(path,"r") as fp:
+    torch.load(fp, map_location="cpu")
