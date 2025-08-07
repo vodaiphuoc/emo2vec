cb3785fe18eca7d1f6820722ae0fb3fb9f56041e
vodaiphuoc <phuocvodn98@gmail.com>
2025-08-07 17:43:29 +0700

    main

--- main.py
+++ main.py
@@ -4,16 +4,30 @@ from model import (
     load_pretrained_model,
     get_pretrain_config
 )
-
+import torch
+from train_utils.loader import get_dataloader
+from train_utils.training_cfg import TrainingConfig
 
 local_dir = download_repo_from_hf()
 pretrain_cfg = get_pretrain_config(download_dir=local_dir)
 pretrain_state_dict = load_pretrained_model(download_dir= local_dir)
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-E2VftModel(
+model = E2VftModel(
     head_dim = 100, 
     num_classes = 5, 
     pretrain_cfg = pretrain_cfg,
     pretrain_state_dict = pretrain_state_dict
-)
+).to(device)
+
+traininig_config = TrainingConfig()
+
+train_dl, test_dl = get_dataloader(training_config= traininig_config)
+
+
+for inputs, labels in train_dl:
+    inputs = {k: v.to(device) for k,v in inputs}
+    labels = labels.to(device)
+
+    model(**inputs)
