fcea5b0c130d830fc98fc8044ee3dfc7f046e32e
vodaiphuoc <phuocvodn98@gmail.com>
2025-08-08 14:41:40 +0700

    reduct batch size

--- train_utils/training_cfg.py
+++ train_utils/training_cfg.py
@@ -3,7 +3,7 @@ import dataclasses
 @dataclasses.dataclass(frozen=True)
 class TrainingConfig:
     test_size: float = 0.3
-    batch_size: int = 8
+    batch_size: int = 4
     learning_rate: float = 0.001
     num_workers: int = 2
     prefetch_factor: int = 2
