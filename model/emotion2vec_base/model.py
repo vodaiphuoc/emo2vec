0c58fd946deac9ed25987e20ea44e728906233f0
vodaiphuoc <phuocvodn98@gmail.com>
2025-08-08 16:49:29 +0700

    dont use mask

--- model/emotion2vec_base/model.py
+++ model/emotion2vec_base/model.py
@@ -93,7 +93,7 @@ class Emotion2vec(torch.nn.Module):
             id=None,
             mode=None,
             padding_mask=None,
-            mask=True,
+            mask=False,
             features_only=False,
             force_remove_masked=False,
             remove_extra_tokens=True,
