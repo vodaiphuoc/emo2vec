072039e5026ced4582987baf1dc0e53a417c04ff
vodaiphuoc <phuocvodn98@gmail.com>
2025-08-08 19:19:00 +0700

    change params

--- train_utils/training_cfg.py
+++ train_utils/training_cfg.py
@@ -3,8 +3,8 @@ import dataclasses
 @dataclasses.dataclass(frozen=True)
 class TrainingConfig:
     test_size: float = 0.3
-    batch_size: int = 8
-    learning_rate: float = 0.0001
+    batch_size: int = 16
+    learning_rate: float = 0.001
     num_workers: int = 2
     prefetch_factor: int = 2
     num_epochs: int  = 20
