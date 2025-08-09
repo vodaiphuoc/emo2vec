10876bf3b8252a858095e1e526bec9cb60d68de0
vodaiphuoc <phuocvodn98@gmail.com>
2025-08-09 14:26:03 +0700

    update params for lr schedular

--- train_utils/training_cfg.py
+++ train_utils/training_cfg.py
@@ -4,7 +4,9 @@ import dataclasses
 class TrainingConfig:
     test_size: float = 0.25
     batch_size: int = 128
-    learning_rate: float = 0.001
+    init_learning_rate: float = 0.001
+    min_learning_rate: float = 0.00001
+    lr_scheduler_epochs_skip: int = 2
     num_workers: int = 2
     prefetch_factor: int = 2
     num_epochs: int  = 40
